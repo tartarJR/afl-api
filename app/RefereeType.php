82c3ae491cba9101cb3664594e372ccb794be183
tartarJR <mustafaogun.ozturk@gmail.com>
2018-05-03 16:23:37 +0300

    created empty models and migrations

---
+++ app/RefereeType.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class RefereeType extends Model
+{
+    //
+}
---
+++ database/migrations/2018_05_03_131603_create_coach_types_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCoachTypesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('coach_types', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('coach_types');
+    }
+}
