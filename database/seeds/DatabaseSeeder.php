edc3ed97568ecd250862e03fa45bbc231b3446a5
tartarJR <mustafaogun.ozturk@gmail.com>
2018-05-07 13:58:20 +0300

    RefereeTypeSeeder added

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -16,5 +16,6 @@ class DatabaseSeeder extends Seeder
         // $this->call(PositionsTableSeeder::class);
         // $this->call(RoleTableSeeder::class);
         // $this->call(CoachTypeTableSeeder::class);
+        // $this->call(RefereeTypeSeeder::class);
     }
 }
---
+++ database/seeds/RefereeTypeSeeder.php
@@ -0,0 +1,58 @@
+<?php
+
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class RefereeTypeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('referee_types')->insert([
+            'type' => 'Referee',
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+        ]);
+
+        DB::table('referee_types')->insert([
+            'type' => 'Umpire',
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+        ]);
+
+        DB::table('referee_types')->insert([
+            'type' => 'Down Judge',
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+        ]);
+
+        DB::table('referee_types')->insert([
+            'type' => 'Line Judge',
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+        ]);
+
+        DB::table('referee_types')->insert([
+            'type' => 'Field Judge',
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+        ]);
+
+        DB::table('referee_types')->insert([
+            'type' => 'Side Judge',
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+        ]);
+
+        DB::table('referee_types')->insert([
+            'type' => 'Back Judge',
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+        ]);
+    }
+}
