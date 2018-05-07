d0a1a940d0f37f379a2d0cf27faf2c99299b8965
tartarJR <mustafaogun.ozturk@gmail.com>
2018-05-07 09:45:07 +0300

    added coach table migrations for FKs

---
+++ database/migrations/2018_05_07_064132_add_coach_type_id_to_coach.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddCoachTypeIdToCoach extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('coaches', function (Blueprint $table) {
+            $table->foreign('coach_type_id')->references('id')->on('coach_types');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('coaches', function (Blueprint $table) {
+            $table->dropForeign('coach_type_id');
+        });
+    }
+}
