f1b473c5c7077f677e4d741b393a04935d245cbb
tartarJR <mustafaogun.ozturk@gmail.com>
2018-05-07 12:12:59 +0300

    added unit_id fk to coach_types migration

---
+++ database/migrations/2018_05_07_091026_add_unit_id_fk_to_coach_type.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddUnitIdFkToCoachType extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('coach_types', function (Blueprint $table) {
+            $table->unsignedInteger('unit_id')->after('type');
+            $table->foreign('unit_id')->references('id')->on('units');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('coach_types', function (Blueprint $table) {
+            $table->dropForeign('coach_types_unit_id_foreign');
+            $table->dropColumn('unit_id');
+        });
+    }
+}
