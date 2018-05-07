2d2c62094f4692fe824671ef39878de91b71343b
tartarJR <mustafaogun.ozturk@gmail.com>
2018-05-07 11:59:30 +0300

    added RoleTableSeeder

---
+++ database/seeds/RoleTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class RoleTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('roles')->insert([
+            'name' => 'Super Admin',
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+        ]);
+
+        DB::table('roles')->insert([
+            'name' => 'Admin',
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+        ]);
+    }
+}
