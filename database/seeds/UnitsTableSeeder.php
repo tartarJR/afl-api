3c54a64c4add9e99c4afaef80f061d29e39721b9
tartarJR <mustafaogun.ozturk@gmail.com>
2018-05-07 11:18:15 +0300

    added UnitsTableSeeder

---
+++ database/seeds/UnitsTableSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+
+class UnitsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('units')->insert([
+            'name' => 'Offensive Unit',
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+        ]);
+
+        DB::table('units')->insert([
+            'name' => 'Defensive Unit',
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+        ]);
+
+        DB::table('units')->insert([
+            'name' => 'Special Teams Unit',
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+        ]);
+    }
+}
