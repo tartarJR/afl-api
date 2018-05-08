bbcc6b2b85513ac5a9034a7d64dea53a740271fa
tartarJR <mustafaogun.ozturk@gmail.com>
2018-05-08 10:27:12 +0300

    created CoachesTableSeeder added coach images and arranged img folders

---
+++ database/seeds/CoachTableSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Coach;
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+use Illuminate\Support\Facades\DB;
+
+class CoachTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('coaches')->delete();
+
+        $faker = Faker::create();
+
+        for ($i = 1; $i <= 8; $i++) {
+            for ($j = 1; $j <= 3; $j++) {
+                Coach::create(['first_name' => $faker->firstName('male'),
+                    'last_name' => $faker->lastName,
+                    'birth_date' => $faker->date($format = 'Y-m-d', $max = '1979-06-09'),
+                    'experience' => rand(3, 10),
+                    'img_path' => $faker->image('public/images/coaches', 200, 200, 'people', false, true),
+                    'coach_type_id' => $j,
+                    'team_id' => $i,
+                    'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                    'updated_at' => Carbon::now()->format('Y-m-d H:i:s')]);
+            }
+        }
+    }
+}
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -21,5 +21,6 @@ class DatabaseSeeder extends Seeder
         // $this->call(SeasonsTableSeeder::class);
         // $this->call(TeamsTableSeeder::class);
         // $this->call(UsersTableSeeder::class);
+        // $this->call(CoachTableSeeder::class);
     }
 }
