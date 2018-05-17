616bdc274d8713b692534a3cceee45d7e4756864
tartarJR <mustafaogun.ozturk@gmail.com>
2018-05-17 13:20:59 +0300

    nav bar controller methods added to HomeController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -25,4 +25,44 @@ class HomeController extends Controller
     {
         return view('home');
     }
+
+    public function coach()
+    {
+        return view('coach.index');
+    }
+
+    public function game()
+    {
+        return view('game.index');
+    }
+
+    public function player()
+    {
+        return view('player.index');
+    }
+
+    public function referee()
+    {
+        return view('referee.index');
+    }
+
+    public function report()
+    {
+        return view('report.index');
+    }
+
+    public function season()
+    {
+        return view('season.index');
+    }
+
+    public function team()
+    {
+        return view('team.index');
+    }
+
+    public function week()
+    {
+        return view('week.index');
+    }
 }
