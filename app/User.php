f1a5744d17020f18e652b10c42e76fd984bba45c
tartarJR <mustafaogun.ozturk@gmail.com>
2018-05-17 15:35:02 +0300

    added relationships for Role and User

--- app/User.php
+++ app/User.php
@@ -15,7 +15,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'first_name', 'last_name', 'email', 'password',
     ];
 
     /**
@@ -26,4 +26,9 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function role()
+    {
+        return $this->hasOne(Role::class);
+    }
 }
