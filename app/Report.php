03c04f475d701b8831910989d395920dca128f08
tartarJR <mustafaogun.ozturk@gmail.com>
2018-05-18 14:54:20 +0300

    added Team, Report relationships

--- app/Report.php
+++ app/Report.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Report extends Model
 {
-    //
+    public function teams()
+    {
+        return $this->belongsToMany(Team::class, 'teams_reports');
+    }
 }
--- app/Team.php
+++ app/Team.php
@@ -30,4 +30,9 @@ class Team extends Model
     {
         return $this->hasMany(Player::class);
     }
+
+    public function reports()
+    {
+        return $this->belongsToMany(Report::class, 'teams_reports');
+    }
 }
