08230d2f023dcb799aa7961380b3c0f9b7fd2085
Mustafa Ogün Öztürk <mustafaogun.ozturk@gmail.com>
2018-05-19 11:56:12 +0300

    fixed jumping scroll bar issue

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,6 +22,7 @@
     <!-- Styles -->
     <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
 </head>
 <body>
 <div id="app">
