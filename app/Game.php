cc7875bd69752f43ca405b785c50cd0eefdd0e59
tartarJR <mustafaogun.ozturk@gmail.com>
2018-05-23 12:19:40 +0300

    added guarded fields

--- app/Game.php
+++ app/Game.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Game extends Model
 {
+    protected $guarded = ['id', 'created_at', 'updated_at'];
+
     public function season()
     {
         return $this->belongsTo(Season::class);
