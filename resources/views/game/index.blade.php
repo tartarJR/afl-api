a17ae25cdb67b4ef25d6b4069a06d174d5d65a3f
tartarJR <mustafaogun.ozturk@gmail.com>
2018-05-23 16:05:15 +0300

    added flash message for game creation

--- resources/views/game/index.blade.php
+++ resources/views/game/index.blade.php
@@ -27,6 +27,13 @@
         </div>
     </div>
 
+    @if(Session::has('successMessage'))
+        <div class="alert alert-success">
+            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+            {{ Session::get('successMessage') }}
+        </div>
+    @endif
+
     <div class="row">
         <div class="col-lg-12">
             @if (count($games) === 0)
