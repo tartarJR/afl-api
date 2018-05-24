f3eecb49c6822b7173781ef3aa734a47d799e803
tartarJR <mustafaogun.ozturk@gmail.com>
2018-05-24 10:34:10 +0300

    fixed missing valdation message

--- app/Http/Requests/StoreGameRequest.php
+++ app/Http/Requests/StoreGameRequest.php
@@ -43,7 +43,7 @@ class StoreGameRequest extends FormRequest
             'home_team_id.required' => 'Lütfen ev sahibi takım seçimi yapınız.',
             'away_team_id.required' => 'Lütfen misaifr takım seçimi yapınız.',
             'game_date_time.required' => 'Lütfen tarih ve zaman giriniz.',
-            'game_date_time.date_format' => 'Lütfen uygun tarih ve zaman .',
+            'game_date_time.date_format' => 'Lütfen uygun tarih ve zaman aralığı giriniz.',
             'place.required' => 'Lütfen yer giriniz.',
             'home_team_scored.integer' => 'Ev sahibi takım skor girdisi sayı olmak zorundadır.',
             'away_team_scored.integer' => 'Misafir takım skor girdisi sayı olmak zorundadır.'
