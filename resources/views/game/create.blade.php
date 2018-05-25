9ef367af3e3f65387d9a206345d9e235ef844a32
tartarJR <mustafaogun.ozturk@gmail.com>
2018-05-25 13:43:37 +0300

    added old values to create game form

--- resources/views/game/create.blade.php
+++ resources/views/game/create.blade.php
@@ -33,28 +33,31 @@
         <div class="form-row">
             <div class="form-group col-md-6">
                 <label for="game-date-time-text">Tarih ve Zaman</label>
-                <input type="datetime-local" class="form-control" id="game-date-time-text" name="game_date_time">
+                <input type="datetime-local" class="form-control" id="game-date-time-text" name="game_date_time" value="{{ old('game_date_time') }}">
             </div>
             <div class="form-group col-md-6">
                 <label for="game-place-text">Yer</label>
-                <input type="text" class="form-control" id="game-place-text" name="place">
+                <input type="text" class="form-control" id="game-place-text" name="place" value="{{ old('place') }}">
             </div>
         </div>
         <div class="form-row">
             <div class="form-group col-md-6">
                 <label for="home-team-score-text">Ev Sahibi Takım Skoru</label>
-                <input type="text" class="form-control" id="home-team-score-text" name="home_team_scored">
+                <input type="text" class="form-control" id="home-team-score-text" name="home_team_scored"
+                       value="{{ old('home_team_scored') }}">
             </div>
             <div class="form-group col-md-6">
                 <label for="away-team-score-text">Misafir Takım Skoru</label>
-                <input type="text" class="form-control" id="away-team-score-text" name="away_team_scored">
+                <input type="text" class="form-control" id="away-team-score-text" name="away_team_scored"
+                       value="{{ old('away_team_scored') }}">
             </div>
         </div>
         <div class="form-group">
             <label for="game-summary-text">Maç Özeti</label>
-            <textarea class="form-control" id="game-summary-text" rows="3" name="game_summary"></textarea>
+            <textarea class="form-control" id="game-summary-text" rows="3"
+                      name="game_summary">{{ old('game_summary') }}</textarea>
         </div>
-        <div class="form-group">
+        <div class=" form-group">
             <div class="form-check">
                 <input type="hidden" name="is_played" value="0">
                 {!! Form::checkbox('is_played', 1, false, ['id' => 'is-game-played-check', 'class' => 'form-check-input']) !!}
