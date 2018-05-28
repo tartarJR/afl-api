271da83ea29992ef31be7c8cafc15807f4c7d7b6
tartarJR <mustafaogun.ozturk@gmail.com>
2018-05-28 17:18:05 +0300

    created season request for season form

---
+++ app/Http/Requests/SeasonForm.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Season;
+use Illuminate\Foundation\Http\FormRequest;
+
+class SeasonForm extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'season' => 'required|unique:seasons',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'season.required' => 'Lütfen sezon seçimi yapınız.',
+            'season.unique' => 'Bu sezon daha önce yaratıldı.',
+        ];
+    }
+
+    public function saveGame()
+    {
+        Season::create($this->all());
+    }
+
+    public function updateGame($id)
+    {
+        $game = Season::findOrFail($id);
+        $game->fill($this->all());
+        $game->save();
+    }
+}
