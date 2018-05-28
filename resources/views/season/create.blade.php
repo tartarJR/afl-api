9764154b36ad3ace11c4c977fa43197bff82de6d
tartarJR <mustafaogun.ozturk@gmail.com>
2018-05-28 16:53:42 +0300

    added create season form

---
+++ resources/views/season/create.blade.php
@@ -0,0 +1,45 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <h5>Sezon Oluşturma Formu</h5>
+
+    <div class="mt-3">
+
+        {!! Form::open(['route' => 'seasons.store']) !!}
+
+        {{ csrf_field() }}
+
+        <div class="form-row">
+            <div class="form-group col-md-6">
+                <label for="season-select">Sezon</label>
+                <select id="season-select" class="form-control" name="season">
+                    <option>Lütfen sezon seçiniz</option>
+                    @for($year = 2010; $year<2026; $year++)
+                        <option value="{{ strval($year) . '/' .strval($year + 1) }}">{{ strval($year) . '/' .strval($year + 1) }}</option>
+                    @endfor
+                </select>
+            </div>
+        </div>
+
+        <div class="form-group">
+            <button type="submit" class="btn btn-primary">Sezon Oluştur</button>
+        </div>
+
+        {!! Form::close() !!}
+
+    </div>
+
+    @if(count($errors))
+        <div class="form-group">
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        </div>
+    @endif
+
+@endsection
