063fc566da3ba781039effc766ddd1a479c6a502
tartarJR <mustafaogun.ozturk@gmail.com>
2018-05-29 10:50:17 +0300

    created season edit form

---
+++ resources/views/season/edit.blade.php
@@ -0,0 +1,47 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <h5>Sezon Güncelleme Formu</h5>
+
+    <div class="mt-3">
+
+        {!! Form::model($season, ['method'=>'patch', 'route' => ['seasons.update', $season->id]]) !!}
+
+        {{ csrf_field() }}
+
+        <div class="form-row">
+            <div class="form-group col-md-6">
+                <label for="season-select">Sezon</label>
+                <select id="season-select" class="form-control" name="season">
+                    <option value="">Lütfen sezon seçiniz</option>
+                    @for($year = 2010; $year<2026; $year++)
+                        <option value="{{ strval($year) . '/' .strval($year + 1) }}" {{ strval($year) . '/' .strval($year + 1) == $season->season ? 'selected="selected"' : '' }}>
+                            {{ strval($year) . '/' .strval($year + 1) }}
+                        </option>
+                    @endfor
+                </select>
+            </div>
+        </div>
+
+        <div class="form-group">
+            <button type="submit" class="btn btn-primary">Sezon Güncelle</button>
+        </div>
+
+        {!! Form::close() !!}
+
+    </div>
+
+    @if(count($errors))
+        <div class="form-group">
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        </div>
+    @endif
+
+@endsection
