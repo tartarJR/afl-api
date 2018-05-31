e6b173a218f82a7237823d89c8f5ed3348ed99f0
tartarJR <mustafaogun.ozturk@gmail.com>
2018-05-31 14:50:32 +0300

    localized datepicker

--- public/js/datepickers.js
+++ public/js/datepickers.js
@@ -1,5 +1,9 @@
 $(document).ready(function () {
     $(function () {
-        $('#birth-date').datepicker();
+        $('#birth-date').datepicker({
+            dateFormat: 'dd/mm/yy',
+            monthNames: ["Ocak", "Şubat", "Mart", "Nisan", "Mayıs", "Haziran", "Temmuz", "Ağustos", "Eylül", "Ekim", "Kasım", "Aralık"],
+            dayNamesMin: ["Pa", "Pt", "Sl", "Ça", "Pe", "Cu", "Ct"]
+        });
     });
 });
