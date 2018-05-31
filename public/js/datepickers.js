1e3df05085752ea2b454ad6b8adc79e91f4b737f
tartarJR <mustafaogun.ozturk@gmail.com>
2018-05-31 16:53:28 +0300

    prevented typing in datepicker input

--- public/js/datepickers.js
+++ public/js/datepickers.js
@@ -1,9 +1,10 @@
 $(document).ready(function () {
-    $(function () {
-        $('#birth-date').datepicker({
-            dateFormat: 'dd/mm/yy',
-            monthNames: ["Ocak", "Şubat", "Mart", "Nisan", "Mayıs", "Haziran", "Temmuz", "Ağustos", "Eylül", "Ekim", "Kasım", "Aralık"],
-            dayNamesMin: ["Pa", "Pt", "Sl", "Ça", "Pe", "Cu", "Ct"]
-        });
+    $('#birth-date').datepicker({
+        dateFormat: 'dd/mm/yy',
+        monthNames: ["Ocak", "Şubat", "Mart", "Nisan", "Mayıs", "Haziran", "Temmuz", "Ağustos", "Eylül", "Ekim", "Kasım", "Aralık"],
+        dayNamesMin: ["Pa", "Pt", "Sl", "Ça", "Pe", "Cu", "Ct"]
+    }).on('keypress paste', function (e) {
+        e.preventDefault();
+        return false;
     });
 });
