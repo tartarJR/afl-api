d7b6526fb547caa1a0f4f0682c3ff2bc573134a6
tartarJR <mustafaogun.ozturk@gmail.com>
2018-06-05 17:13:22 +0300

    ReportForm request created rules, msgs and store method is done

---
+++ app/Http/Requests/ReportForm.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Intervention\Image\Facades\Image;
+use App\Models\Report;
+
+class ReportForm extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+     public function rules()
+     {
+         $rules = ['title' => 'required',
+             'content' => 'required',
+             'related_teams' => 'required'
+         ];
+
+         if ($this->isMethod('post')) {
+             $rules['img_path'] = 'required|mimes:png';
+         } else {
+             $rules['img_path'] = 'mimes:png';
+         }
+
+         return $rules;
+     }
+
+     public function messages()
+     {
+         return [
+             'title.required' => 'Lütfen haber başlığı giriniz.',
+             'content.required' => 'Bu haber içeriği giriniz.',
+             'related_teams.required' => 'Lütfen haber ile ilişkili takımları giriniz.',
+             'img_path.required' => 'Lütfen haber için imaj seçiniz.',
+             'img_path.mimes' => 'Haber imajı png formatında olmalıdır.',
+         ];
+     }
+
+     public function saveReport()
+     {
+         $reportImg = $this->file('img_path');
+         $reportImgName = time() . '.' . $reportImg->getClientOriginalExtension();
+         $fileLocation = storage_path('app/public/images/reports/' . $reportImgName);
+         Image::make($reportImg)->resize(400, 400)->save($fileLocation);
+
+         $data = $this->all();
+
+         $lastCreatedReport = Report::create([
+            'title' => $data['title'],
+            'content' => $data['content'],
+            'img_path' => $reportImgName
+         ]);
+
+        $relatedTeamIds = $data['related_teams'];
+
+        foreach($relatedTeamIds as $relatedTeamId) {
+            $lastCreatedReport->teams()->attach($relatedTeamId);
+        }
+     }
+
+     public function updateReport($id)
+     {
+         
+     }
+}
