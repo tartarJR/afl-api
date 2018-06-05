85f3323bb6e9a95d6fdd87a99c346e1a3726ffd1
tartarJR <mustafaogun.ozturk@gmail.com>
2018-06-05 17:08:41 +0300

    added guarded fields and timestamps of relation, Report model

--- app/Models/Report.php
+++ app/Models/Report.php
@@ -6,8 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Report extends Model
 {
+    protected $guarded = ['id', 'created_at', 'updated_at'];
+
     public function teams()
     {
-        return $this->belongsToMany(Team::class, 'teams_reports');
+        return $this->belongsToMany(Team::class, 'teams_reports')->withTimestamps();
     }
 }
