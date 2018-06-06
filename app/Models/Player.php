d20bb03ebc06cc1f286a6ddadebb7d9299c2ec42
tartarJR <mustafaogun.ozturk@gmail.com>
2018-06-06 12:12:56 +0300

    added guarded fields to Player model

--- app/Models/Player.php
+++ app/Models/Player.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Player extends Model
 {
+    protected $guarded = ['id', 'created_at', 'updated_at'];
+
     public function team()
     {
         return $this->belongsTo(Team::class);
