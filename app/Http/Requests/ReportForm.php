16048ca6aad27d0c8bb5b320d8e39b661ef22c08
tartarJR <mustafaogun.ozturk@gmail.com>
2018-06-06 10:37:46 +0300

    updateReport method added to ReportForm

--- app/Http/Requests/ReportForm.php
+++ app/Http/Requests/ReportForm.php
@@ -4,6 +4,8 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 use Intervention\Image\Facades\Image;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Storage;
 use App\Models\Report;
 
 class ReportForm extends FormRequest
@@ -74,6 +76,30 @@ class ReportForm extends FormRequest
 
      public function updateReport($id)
      {
-         
+         $report = Report::findOrFail($id);
+         $data = $this->all();
+
+         if ($this->hasFile('img_path')) {
+
+             $reportImg = $this->file('img_path');
+             $reportImgName = time() . '.' . $reportImg->getClientOriginalExtension();
+             $fileLocation = storage_path('app/public/images/reports/' . $reportImgName);
+             Image::make($reportImg)->resize(400, 400)->save($fileLocation);
+
+             $oldReportImg = $report->img_path;
+             $data['img_path'] = $reportImgName;
+
+             File::delete(storage_path('app/public/images/reports/' . $oldReportImg));
+
+             $report->img_path = $reportImgName;
+         }
+
+         $report->title = $data['title'];
+         $report->content = $data['content'];
+
+         $report->save();
+
+         $relatedTeamIds = $data['related_teams'];
+         $report->teams()->sync($relatedTeamIds); // updateExistingPivot did not work
      }
 }
