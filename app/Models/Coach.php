17e43e4b2b95f314ef6bce6e0fdc7e85511fb378
tartarJR <mustafaogun.ozturk@gmail.com>
2018-06-06 11:58:51 +0300

    Coach model accessors

--- app/Models/Coach.php
+++ app/Models/Coach.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
 class Coach extends Model
@@ -14,5 +15,15 @@ class Coach extends Model
     public function team()
     {
         return $this->belongsTo(Team::class);
+    }
+
+    public function getFullNameAttribute()
+    {
+        return $this->first_name . ' ' . $this->last_name;
+    }
+
+    public function getAgeAttribute()
+    {
+        return Carbon::parse($this->attributes['birth_date'])->age;
     }
 }
