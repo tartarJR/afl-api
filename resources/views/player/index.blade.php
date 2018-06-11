7453cdc5ab855521a62acaf9c5b37c51744b3259
tartarJR <mustafaogun.ozturk@gmail.com>
2018-06-11 14:39:35 +0300

    updated index page of players

--- resources/views/player/index.blade.php
+++ resources/views/player/index.blade.php
@@ -17,38 +17,20 @@
                     <thead>
                     <tr>
                         <th scope="col">Ad</th>
-                        <th scope="col">Yaş</th>
                         <th scope="col">Uyruk</th>
-                        <th scope="col">Doğum Yeri</th>
-                        <th scope="col">Boy</th>
-                        <th scope="col">Kilo</th>
-                        <th scope="col">Forma Numarası</th>
-                        <th scope="col">Tecrübe</th>
-                        <th scope="col">Birincil Pozisyon</th>
-                        <th scope="col">İkincil Pozisyon</th>
                         <th scope="col">Takım</th>
+                        <th scope="col">Detay</th>
+                        <th scope="col">Sil</th>
+                        <th scope="col">Güncelle</th>
                     </tr>
                     </thead>
                     <tbody>
                     @foreach ($players as $player)
                         <tr>
                             <td>{{ $player->full_name }}</td>
-                            <td>{{ $player->age }}</td>
                             <td>{{ $player->nationality }}</td>
-                            <td>{{ $player->hometown }}</td>
-                            <td>{{ $player->height_with_unit }}</td>
-                            <td>{{ $player->weight_with_unit }}</td>
-                            <td>{{ $player->jersey_number }}</td>
-                            <td>{{ $player->experience }}</td>
-                            <td>{{ $player->primaryPosition->name }}</td>
-                            <td>
-                                @if (isset($player->secondaryPosition))
-                                    {{ $player->secondaryPosition->name }}
-                                @else
-                                    -
-                                @endif
-                            </td>
                             <td>{{ $player->team->name }}</td>
+                            <td>{{ link_to_route('players.show', 'Detaylar', $player->id, ['class' => 'btn btn-primary']) }}</td>
                             <td>{{ link_to_route('players.edit', 'Güncelle', $player->id, ['class' => 'btn btn-primary']) }}</td>
                             <td>
                                 <form method="POST" action="{{ route('players.destroy', $player->id) }}">
@@ -65,7 +47,7 @@
         </div>
     </div>
     <div class="d-flex justify-content-between bd-highlight mb-3">
-        <div class="p-2 bd-highlight"><a href="{{ route('coaches.create') }}"><p>Yeni bir oyuncu yarat</p></a></div>
+        <div class="p-2 bd-highlight"><a href="{{ route('players.create') }}"><p>Yeni bir oyuncu yarat</p></a></div>
         <div class="p-2 bd-highlight">{{ $players->links() }}</div>
     </div>
 
